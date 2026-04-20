93cc9e168540047d2ff40c4b2b30755781c5a2e9
SOR2171 <sor2171@foxmail.com>
2026-04-20 05:56:05 +0000

    feat: better FlowLimitFilter.kt with resource

--- backend/src/main/kotlin/com/itstudio/careerpp/filter/FlowLimitFilter.kt
+++ backend/src/main/kotlin/com/itstudio/careerpp/filter/FlowLimitFilter.kt
@@ -3,6 +3,7 @@ package com.itstudio.careerpp.filter
 import com.itstudio.careerpp.entity.RestBean
 import com.itstudio.careerpp.entity.RestBean.Companion.toJsonString
 import com.itstudio.careerpp.utils.Const
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.annotation.Order
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate
 import org.springframework.http.HttpStatus
@@ -17,7 +18,15 @@ import java.time.Duration
 @Component
 @Order(Const.FLOW_LIMIT_ORDER)
 class FlowLimitFilter(
-    private val template: ReactiveStringRedisTemplate
+    private val template: ReactiveStringRedisTemplate,
+
+    @param:Value($$"${app.flow-limit.count}")
+    private val limitCount: Int,
+    @param:Value($$"${app.flow-limit.limit-time}")
+    private val limitTime: Int,
+    @param:Value($$"${app.flow-limit.ban-time}")
+    private val expireTime: Int,
+    
 ) : WebFilter {
 
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
@@ -49,21 +58,22 @@ class FlowLimitFilter(
             if (isBlocked) {
                 Mono.just(false)
             } else {
-                template.hasKey(counterKey).flatMap { hasCounter ->
-                    if (hasCounter) {
-                        template.opsForValue().increment(counterKey).flatMap { count ->
-                            if (count > 10) {
-                                template.opsForValue()
-                                    .set(blockKey, "1", Duration.ofMinutes(1))
-                                    .thenReturn(false)
-                            } else {
-                                Mono.just(true)
-                            }
-                        }
-                    } else {
+                template.opsForValue().increment(counterKey).flatMap { count ->
+                    if (count == 1L) {
+                        template.expire(
+                            counterKey,
+                            Duration.ofSeconds(limitTime.toLong())
+                        ).thenReturn(true)
+                    } else if (count > limitCount) {
                         template.opsForValue()
-                            .set(counterKey, "1", Duration.ofSeconds(3))
-                            .thenReturn(true)
+                            .set(
+                                blockKey,
+                                "1",
+                                Duration.ofSeconds(expireTime.toLong())
+                            )
+                            .thenReturn(false)
+                    } else {
+                        Mono.just(true)
                     }
                 }
             }
